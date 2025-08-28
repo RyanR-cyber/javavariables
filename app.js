06961a8ce1df3d6c57ad88696c157f45ad47f2b1
RyanR-cyber <ozzy19992@gmail.com>
2025-08-27 19:36:14 -0500

    intlize

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
-
-{
-    let x = 10; // 'x' is declared with 'let' inside this block, so it is block-scoped.
-}
-
-
-
-console.log(x); // Output: Hello, because 'text' is accessible in the current scope.
+let video = "youtube";
+console.log(video);
