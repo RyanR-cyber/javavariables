c161fcf0388bc63d9a2a3ee26aca20dd21daa52b
RyanR-cyber <ozzy19992@gmail.com>
2025-08-27 19:26:35 -0500

    intlize

--- app.js
+++ app.js
@@ -2,11 +2,7 @@
 {
     let x = 10; // 'x' is declared with 'let' inside this block, so it is block-scoped.
 }
-// console.log(x); // This would throw a ReferenceError because 'x' is not accessible outside the block.
 
 
 
-let text = "Hello"; // 'text' is declared with 'let', so it is block-scoped to the current script or function.
-
-
-console.log(text); // Output: Hello, because 'text' is accessible in the current scope.
+console.log(x); // Output: Hello, because 'text' is accessible in the current scope.
